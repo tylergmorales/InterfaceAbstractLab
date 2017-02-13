7f51aa8a7c63691187da4e475508e9b38283f48c
Tyler <Tyler@Tyler>
2017-02-12 23:41:45 -0600

    lab 2 done

---
+++ src/Course.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tyler
+ */
+public interface Course {
+    public String setCourseName();
+    public String getCourseName();
+    public String setCourseNumber();
+    public String getCourseNumber();
+    public double setCredits();
+    public double getCredits();
+    public String setPrerequisites();
+    public String getPrerequisites();
+}
